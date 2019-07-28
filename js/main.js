46351ec8ecb61013ee8c40011f53c814671e63a1
leandro <le.mlima@hotmail.com>
2019-07-28 02:45:27 -0300

    atualizacao da home e da pagina interna

--- js/main.js
+++ js/main.js
@@ -572,5 +572,15 @@ jQuery(window).load(function(){
     jQuery('.produto-imagem.zoom-off').attr('data-toggle', '');
     
     cssVars();
+    
 
 });
+function process(quant){
+    var value = parseInt(document.getElementById("quant").value);
+    value+=quant;
+    if(value < 1){
+      document.getElementById("quant").value = 1;
+    }else{
+    document.getElementById("quant").value = value;
+    }
+}
